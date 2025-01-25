d177496d947434d55dfda46e4d5fb583bfe592e7
Chris Holman <holmanc101@gmail.com>
2025-01-24 19:34:20 -0500

    Completed Programming Assignment 2 that created a function to return the inverse of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,33 @@
-## Put comments here that give an overall description of what your
+
+#Edit the R file contained in the git repository and place your solution in that file (please do not rename the file).
+
+#Commit your completed R file into YOUR git repository and push your git branch to the GitHub repository under your account.
+
+#Submit to Coursera the URL to your GitHub repository that contains the completed R code for the assignment.
+
+
+
+
+
+
+
+##Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
@@ -12,4 +35,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
 }
